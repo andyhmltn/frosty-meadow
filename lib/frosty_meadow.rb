a28ae061bc0d72fad427bc8243bccf6d0cbb9731
Andy Hamilton <andyhmltn@gmail.com>
2013-02-25 15:41:26 +0000

    Improved the gem code to make it less cluttered and more "ruby"ish

--- lib/frosty_meadow.rb
+++ lib/frosty_meadow.rb
@@ -2,19 +2,21 @@ require 'json'
 
 class FrostyMeadow
 	def self.generate params = {}
-		adjectives = (params[:adjectives].nil?)? self.get_default_words['adjectives'] : params[:adjectives]
-		nouns = (params[:nouns].nil?)? self.get_default_words['nouns'] : params[:nouns]
+		words = get_words params
+
+		adjectives = words['adjectives']
+		nouns      = words['nouns']
 
 		result = "#{adjectives[rand(adjectives.length)]} #{nouns[rand(nouns.length)]}"
-
-		return (params[:underscored].nil? || params[:underscored] == false)? result : self.to_underscored(result)
+		
+		return (params[:underscored] && self.to_underscored(result)) || result
 	end
 
 	def self.to_underscored result
 		result.gsub(/ +/, '_')
 	end
 
-	def self.get_default_words 
+	def self.get_words params = {}
 		file_path =  File.join(File.dirname(__FILE__), 'data/words.json')
 		file = File.new file_path, "r"
 
@@ -23,6 +25,11 @@ class FrostyMeadow
 			file_contents << line
 		end
 
-		JSON.parse(file_contents)
+		words = JSON.parse(file_contents)
+
+		words['adjectives'] = params[:adjectives] unless params[:adjectives].nil?
+		words['nouns'] = params[:nouns] unless params[:nouns].nil?
+
+		return words
 	end
 end
