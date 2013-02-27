8dbaf3bdfc026688d67e5cf37915092c3d5e8e4b
Andy Hamilton <andyhmltn@gmail.com>
2013-02-27 13:00:02 +0000

    Added the initial tests

--- spec/frosty_meadow_spec.rb
+++ spec/frosty_meadow_spec.rb
@@ -19,4 +19,20 @@ describe FrostyMeadow do
 			FrostyMeadow.generate.split.size.should eql 2
 		end
 	end
+
+	describe "#hex_string" do
+		it "generates a 5 letter hexadecimal string by default" do
+			string = FrostyMeadow.hex_string
+
+			string.should =~ /^[0-9A-F]+$/i
+			string.length.should eql 5
+		end
+
+		it "generates a hexadecimal string with the provided length" do
+			FrostyMeadow.hex_string(1).length.should eql 1
+			FrostyMeadow.hex_string(2).length.should eql 2
+			FrostyMeadow.hex_string(3).length.should eql 3
+			FrostyMeadow.hex_string(100).length.should eql 100 
+		end
+	end
 end
